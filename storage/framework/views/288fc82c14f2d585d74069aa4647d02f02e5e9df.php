8efc46e14fef82ff0d1d397e03522b3605f5fb0e
junkoyasu <160372603+junkoyasu@users.noreply.github.com>
2024-06-26 19:43:54 +0900

    laravel.todo.app

---
+++ storage/framework/views/288fc82c14f2d585d74069aa4647d02f02e5e9df.php
@@ -0,0 +1,2 @@
+[<?php echo e($slot); ?>](<?php echo e($url); ?>)
+<?php /**PATH /Applications/MAMP/htdocs/laravel-todo-app/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4ee567671006c039fb07a665401836b7d3ff213a.php
@@ -0,0 +1,5 @@
+<footer>
+     <div class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px; z-index: auto;">
+         <p class="text-muted small mb-0">&copy; <?php echo e(config('app.name', 'Laravel')); ?> All rights reserved.</p>
+     </div> 
+ </footer><?php /**PATH /Applications/MAMP/htdocs/laravel-todo-app/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
